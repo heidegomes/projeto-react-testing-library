fefe2c754ef0488e8743f3a1766343a013854a1a
heidegomes <heidegc@hotmail.com>
2022-10-19 15:54:12 -0300

    Requisito 1 ajustes

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,19 +1,23 @@
-import React from "react";
-import { render, screen } from "@testing-library/react";
-import userEvent from "@testing-library/user-event";
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+// import { Router } from 'react-router-dom';
 import App from '../App';
-import { Router } from 'react-router-dom';
-import renderWithRouter from "../renderWithRouter";
+import renderWithRouter from '../renderWithRouter';
 
 test('Teste se o topo da aplicação contém um conjunto fixo de links de navegação', () => {
   renderWithRouter(<App />);
-  const nav = screen.getByRole('link', {name: /Home/i, name: /About/i, name: /Favorite Pokémons/i  });
-  expect(nav).toBeInTheDocument();
+  const home = screen.getByRole('link', { name: /Home/i });
+  const about = screen.getByRole('link', { name: /About/i });
+  const favorite = screen.getByRole('link', { name: /Favorite Pokémons/i });
+  expect(home).toBeInTheDocument();
+  expect(about).toBeInTheDocument();
+  expect(favorite).toBeInTheDocument();
 });
 
 test('Teste se a aplicação é redirecionada para a página inicial, na URL / ao clicar no link Home da barra de navegação', () => {
   const { history } = renderWithRouter(<App />);
-  const home = screen.getByRole('link', { name: /Home/i});
+  const home = screen.getByRole('link', { name: /Home/i });
   userEvent.click(home);
   const { pathname } = history.location;
   expect(pathname).toBe('/');
@@ -23,7 +27,7 @@ test('Teste se a aplicação é redirecionada para a página de About, na URL /a
   const { history } = renderWithRouter(<App />);
   const about = screen.getByRole('link', { name: /About/i });
   userEvent.click(about);
-  const { location: { pathname } } = history; 
+  const { location: { pathname } } = history;
   expect(pathname).toBe('/about');
 });
 
@@ -37,9 +41,9 @@ test('Teste se a aplicação é redirecionada para a página de Pokémons Favori
 
 test('Teste se a aplicação é redirecionada para a página Not Found ao entrar em uma URL desconhecida', () => {
   const { history } = renderWithRouter(<App />);
-  const titleHome = screen.getByRole('heading', {level: 1});
+  const titleHome = screen.getByRole('heading', { level: 1 });
   expect(titleHome).toBeInTheDocument();
-  history.push('/qualquerCoisa')
+  history.push('/qualquerCoisa');
   const { location: { pathname } } = history;
   expect(pathname).toBe('/qualquerCoisa');
 });
