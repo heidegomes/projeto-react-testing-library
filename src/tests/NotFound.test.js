c35585881996076fcd085d8146cd9239f4d80836
heidegomes <heidegc@hotmail.com>
2022-10-19 21:38:34 -0300

    Requisito 4 feito!

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,17 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import NotFound from '../pages/NotFound';
+
+test('Teste se a página contém um heading h2 com o texto `Page requested not found`', () => {
+  renderWithRouter(<NotFound />);
+  const msgNotFound = screen.getByRole('heading', { level: 2, name: /Page requested not found/i });
+  expect(msgNotFound).toBeInTheDocument();
+});
+
+test('Teste se a página contém a seguinte imagem de uma Pokédex', () => {
+  renderWithRouter(<NotFound />);
+  const imgNotFound = screen.getByAltText('Pikachu crying because the page requested was not found');
+  const imgExpect = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+  expect(imgNotFound.src).toBe(imgExpect);
+});
